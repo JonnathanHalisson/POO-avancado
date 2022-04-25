8fd1a089b84d736542cbed91a83a7bbc5f4fcc51
JonnathanHalisson <jonnathan.halisson@gmail.com>
2022-04-25 16:18:56 -0300

    Terceira atividade usando composição

---
+++ Terceira Atividade - Composição/src/Main.java
@@ -0,0 +1,40 @@
+import Horario.Horario;
+import Produto.Produto;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        @SuppressWarnings("resource")
+
+        Scanner entrada = new Scanner(System.in);
+        int hora, minuto, segundo;
+        float preco;
+        String dia;
+        Horario horario;
+        Produto produto;
+
+        System.out.print("Digite a hora: ");
+        hora = entrada.nextInt();
+
+        System.out.print("Digite o minuto: ");
+        minuto = entrada.nextInt();
+
+        System.out.print("Digite o segundo: ");
+        segundo = entrada.nextInt();
+
+        System.out.print("Digite o dia: ");
+        dia = entrada.next();
+
+        System.out.print("Preco do produto: ");
+        preco = entrada.nextFloat();
+
+        horario = new Horario(dia, hora, minuto, segundo);
+        produto = new Produto(preco);
+
+        produto.calculaPreco(horario, horario);
+
+        System.out.println("Preco do produto: "+produto.getPreco());
+
+    }
+}
---
+++ Terceira Atividade - Composição/src/Produto/Produto.java
@@ -0,0 +1,30 @@
+package Produto;
+
+import Horario.Horario;
+
+public class Produto {
+
+    private float preco;
+
+    public Produto(float preco){
+        this.preco = preco;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public void calculaPreco(Horario hora, Horario dia){
+        if(hora.getHora()>20 && hora.getHora()<24){
+            System.out.println("**********GANHOU DESCONTO**********");
+            preco = preco - (preco * 0.10f);
+        } else if(dia.getDia().equals("Sabado") || dia.getDia().equals("sabado") || dia.getDia().equals("Domingo") || dia.getDia().equals("domingo")){
+                System.out.println("**********GANHOU DESCONTO**********");
+                preco = preco - (preco * 0.10f);
+        }
+    }
+}
